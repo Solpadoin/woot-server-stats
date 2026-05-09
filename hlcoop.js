b5974b3a5258cdf02569bc10749c6afd9332cec1
Bohdan <44276347+Solpadoin@users.noreply.github.com>
2026-05-09 17:33:28 +0300

    Update hlcoop.js Fix open link setup

--- hlcoop.js
+++ hlcoop.js
@@ -11,7 +11,7 @@ var g_player_clients = {}; // client details by steam id
 const DISPLAY_SERVER_NAME = "Half-Life Co-op | Custom maps";
 var g_server_name = DISPLAY_SERVER_NAME;
 var g_steam_openid_url = "";
-var g_dashboard_url = "woot-server-stats/";
+var g_dashboard_url = "/woot-server-stats/";
 var g_selected_map = "";
 var g_reset_table_timer;
 var g_mouseover_state = false;
@@ -1900,8 +1900,18 @@ function setup_openid_link() {
 		return;
 	}
 	
-	let return_to = window.location.origin + window.location.pathname;	
-	let openid_link = "https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=" + return_to + "&openid.realm=" + return_to +"&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select";
+	let return_to = window.location.origin + window.location.pathname;
+	let realm = window.location.origin;
+
+	let openid_link =
+		"https://steamcommunity.com/openid/login?" +
+		"openid.ns=http://specs.openid.net/auth/2.0" +
+		"&openid.mode=checkid_setup" +
+		"&openid.return_to=" + encodeURIComponent(return_to) +
+		"&openid.realm=" + encodeURIComponent(realm) +
+		"&openid.identity=http://specs.openid.net/auth/2.0/identifier_select" +
+		"&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select";
+
 	document.getElementById("login_but").setAttribute("href", openid_link);
 }
 
